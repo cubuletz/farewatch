808fa6dfb0337e71df65f009f290a22a706f5994
cubuletz <cristianciobanu1982@yahoo.co.uk>
2026-03-01 18:46:43 +0000

    Update alerts with test-email endpoint

--- backend/routes/alerts.js
+++ backend/routes/alerts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const nodemailer = require('nodemailer')
-const { db } = require('../database')
+const db = require('../database')
 
 // Save an alert
 router.post('/alerts', (req, res) => {
